3ddcd367cd85201fe6a28b3a02c2fb9d6eef9884
zahraa <Zahraa9597>
2024-12-24 09:11:53 -0800

    created Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+/** * The Main Class to Run.
+ */public class Main {
+    /**     * The entry point of application.
+     *     * @param args system arguments
+     * @throws IOException            the io exception     * @throws ClassNotFoundException the class not found exception
+     */    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        new Notebook().mainMenu();    }
+}
